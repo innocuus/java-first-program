09a73fd4eaec4c711a0003205384fed4af13d6d1
Jacqui <innocuus12@gmail.com>
2022-10-28 19:51:29 +1100

    a adjusted whitespace -_-

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -11,8 +11,8 @@ public class Finance {
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates", 
-    SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>", 
+    public final static Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
+    SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
     MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
     //Methods
