0faf28a2cf03bd99d266295503b949ead3b62214
Jacqui <innocuus12@gmail.com>
2022-10-28 16:23:21 +1100

    Minor fix

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -45,7 +45,7 @@ public class MortgageCalculator {
 
     public static void main(String[] args) {
         long loanAmount = Long.parseLong(args[0]);
-        int termInYears = Integer.parseInt(args[1]):
+        int termInYears = Integer.parseInt(args[1]);
         float annualRate = Float.parseFloat(args[2]);
 
         MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
